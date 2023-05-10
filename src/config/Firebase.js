91ad50bf0c3c52a3264f57d71217b351c4f1517a
galogico <galogico@gmail.com>
2023-05-10 16:51:51 -0300

    adicionando o gitignore

---
+++ src/config/Firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKfPSiGcYppm-d6AlJhp5F8_af0EPSkdE",
+  authDomain: "taubatexasscouting.firebaseapp.com",
+  projectId: "taubatexasscouting",
+  storageBucket: "taubatexasscouting.appspot.com",
+  messagingSenderId: "151754090896",
+  appId: "1:151754090896:web:3c81a7966130f4dab72b15",
+  measurementId: "G-L6B7DKME6H"
+};
+
+
+const app = initializeApp(firebaseConfig);
---
+++ src/config/PublicFirebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+//put your firebase info here!
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+
+const app = initializeApp(firebaseConfig);
