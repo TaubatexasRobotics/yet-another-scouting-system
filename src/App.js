fa0df071fbf8c7546920eb7b1f607cc7f2998dcf
galogico <galogico@gmail.com>
2023-05-25 21:59:15 -0300

    arrumar o bglh que n ta puxando do server

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import { Auth } from './components/auth';
+import { db } from './config/Firebase';
+import { useEffect, useState } from 'react';
+import { getDocs, collection } from 'firebase/firestore';
 
 function App() {
+  const [teamList, setTeamList] = useState([])
+  const teamsCollectionRef = collection(db, 'teams')
+  
+  useEffect(() =>{
+    const getTeamList = async () =>{
+
+      try{
+      const data = await getDocs()
+      console.log(data());
+      }catch (err){
+        console.error(err)
+      }
+    }
+
+    getTeamList();
+  }, [])
+
   return (
     <div className='app'>
       <Auth/>
